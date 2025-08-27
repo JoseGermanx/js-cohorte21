e5c300c4da87184fc789bf0e951758a64066e30d
José G. Martínez <jgermanmm@gmail.com>
2025-08-27 11:10:26 -0400

    Agregar archivos constantes.js y main.js con funciones de suma, resta y lista de nombres

---
+++ constantes.js
@@ -0,0 +1,14 @@
+
+
+const listaNombres = ["Luis", "Daniela", "Alberto", "José", "Lucas", "Diego"]
+
+function suma(){
+ return 2 + 2
+}
+
+function resta(){
+ return 2 - 2
+}
+
+// commonJS
+module.exports = { listaNombres, suma, resta }
---
+++ main.js
@@ -0,0 +1,12 @@
+
+// utilizar los módulos
+
+const x = require("./constantes.js")
+
+
+const listaNombres = ["Jennifer", "Joselyne", "Juan"]
+
+console.log(listaNombres)
+console.log(x.listaNombres)
+console.log(x.suma())
+console.log(x.resta())
